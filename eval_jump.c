b46e4f34d847f5cbf5e0207f703b482a60a702f6
Yuta Saito <kateinoigakukun@gmail.com>
2021-10-31 00:47:27 +0900

    Replace EC_EXEC_TAG use in rb_ec_exec_end_proc with try-catch style

--- eval_jump.c
+++ eval_jump.c
@@ -107,28 +107,55 @@ exec_end_procs_chain(struct end_proc_data *volatile *procs, VALUE *errp)
     }
 }
 
+// FIXME(katei): Please place it in a suitable file!!!
+enum ruby_tag_type
+rb_try_catch(rb_execution_context_t *ec,
+             void (* b_proc) (VALUE), VALUE data1,
+             enum ruby_tag_type (* r_proc) (VALUE, enum ruby_tag_type), VALUE data2);
+
+struct  rb_ec_exec_end_proc_context {
+    rb_execution_context_t * ec;
+    volatile VALUE errinfo;
+};
+
+static void rb_ec_exec_end_proc_main(VALUE v)
+{
+    struct rb_ec_exec_end_proc_context *ctx = (struct rb_ec_exec_end_proc_context *)v;
+    exec_end_procs_chain(&ephemeral_end_procs, &ctx->ec->errinfo);
+    exec_end_procs_chain(&end_procs, &ctx->ec->errinfo);
+}
+
+static enum ruby_tag_type rb_ec_exec_end_proc_rescue(VALUE v, enum ruby_tag_type state)
+{
+    struct rb_ec_exec_end_proc_context *ctx = (struct rb_ec_exec_end_proc_context *)v;
+    rb_execution_context_t *ec = ctx->ec;
+
+    // FIXME(katei): Should encapsulate _ec and _tag variables?
+    rb_execution_context_t *_ec = ec;
+    struct rb_vm_tag _tag = *ec->tag;
+
+	EC_TMPPOP_TAG();
+    error_handle(ec, state);
+	if (!NIL_P(ec->errinfo)) {
+        ctx->errinfo = ec->errinfo;
+    }
+	EC_REPUSH_TAG();
+    return state;
+}
+
 static void
 rb_ec_exec_end_proc(rb_execution_context_t * ec)
 {
     enum ruby_tag_type state;
-    volatile VALUE errinfo = ec->errinfo;
+    struct rb_ec_exec_end_proc_context ctx = {
+        .ec = ec, .errinfo = ec->errinfo
+    };
 
-    EC_PUSH_TAG(ec);
-    if ((state = EC_EXEC_TAG()) == TAG_NONE) {
-      again:
-	exec_end_procs_chain(&ephemeral_end_procs, &ec->errinfo);
-	exec_end_procs_chain(&end_procs, &ec->errinfo);
-    }
-    else {
-	EC_TMPPOP_TAG();
-        error_handle(ec, state);
-	if (!NIL_P(ec->errinfo)) errinfo = ec->errinfo;
-	EC_REPUSH_TAG();
-	goto again;
-    }
-    EC_POP_TAG();
+    do {
+        state = rb_try_catch(ec, rb_ec_exec_end_proc_main, (VALUE)&ctx, rb_ec_exec_end_proc_rescue, (VALUE)&ctx);
+    } while (state != TAG_NONE);
 
-    ec->errinfo = errinfo;
+    ec->errinfo = ctx.errinfo;
 }
 
 void
