e1762def365456e688fd610eeeebd733938e6dd0
Yuta Saito <kateinoigakukun@gmail.com>
2021-11-07 11:28:05 +0900

    minimize thread_none impl

--- thread_none.h
+++ thread_none.h
@@ -1,41 +1,20 @@
 #ifndef RUBY_THREAD_NONE_H
 #define RUBY_THREAD_NONE_H
 
-#define RB_NATIVETHREAD_LOCK_INIT (void*)(0)
-#define RB_NATIVETHREAD_COND_INIT (void*)(0)
+#define RB_NATIVETHREAD_LOCK_INIT (void)(0)
+#define RB_NATIVETHREAD_COND_INIT (void)(0)
+
+// no-thread impl doesn't use TLS but define this to avoid using tls key
+// based implementation in vm.c
+#define RB_THREAD_LOCAL_SPECIFIER
 
 typedef struct native_thread_data_struct {
-    union {
-        struct list_node ubf;
-        struct list_node gvl;
-    } node;
 } native_thread_data_t;
 
 
 typedef struct rb_global_vm_lock_struct {
-  struct list_head waitq;
 } rb_global_vm_lock_t;
 
+RUBY_EXTERN struct rb_execution_context_struct *ruby_current_ec;
 
-typedef struct native_tls_key {
-  void *value;
-} *native_tls_key_t;
-
-RUBY_SYMBOL_EXPORT_BEGIN
-  RUBY_EXTERN native_tls_key_t ruby_current_ec_key;
-RUBY_SYMBOL_EXPORT_END
-
-
-static inline void *
-native_tls_get(native_tls_key_t key)
-{
-    return key->value;
-}
-
-static inline void
-native_tls_set(native_tls_key_t key, void *ptr)
-{
-  key->value = ptr;
-}
-
-#endif /* RUBY_THREAD_PTHREAD_H */
+#endif /* RUBY_THREAD_NONE_H */
