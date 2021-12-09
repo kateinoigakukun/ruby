536b4093a71a6898c92b0b447575314e29999015
Yuta Saito <kateinoigakukun@gmail.com>
2021-12-09 14:57:46 +0900

    [wasm] wasm/missing.c: fix fake libc funcs to return -1 to follow the spec

--- wasm/missing.c
+++ wasm/missing.c
@@ -4,29 +4,51 @@
 #define WASM_MISSING_LIBC_FUNC __attribute__((weak))
 
 WASM_MISSING_LIBC_FUNC
-int chmod(const char *pathname, int mode) { return 0; }
+int chmod(const char *pathname, int mode) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int chown(const char *pathname, int owner, int group) { return 0; }
+int chown(const char *pathname, int owner, int group) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int dup(int oldfd) { return ENOTSUP; }
+int dup(int oldfd) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int dup2(int oldfd, int newfd) { return ENOTSUP; }
+int dup2(int oldfd, int newfd) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int execl(const char *path, const char *arg, ...) { return ENOTSUP; }
+int execl(const char *path, const char *arg, ...) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int execle(const char *path, const char *arg, ...) { return ENOTSUP; }
+int execle(const char *path, const char *arg, ...) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int execv(const char *path, char *const argv[]) { return ENOTSUP; }
+int execv(const char *path, char *const argv[]) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
 int execve(const char *filename, char *const argv[], char *const envp[]) {
-  return ENOTSUP;
+  errno = ENOTSUP;
+  return -1;
 }
 
 WASM_MISSING_LIBC_FUNC
@@ -45,7 +67,10 @@ WASM_MISSING_LIBC_FUNC
 int getgid(void) { return 0; }
 
 WASM_MISSING_LIBC_FUNC
-char *getlogin(void) { return 0; }
+char *getlogin(void) {
+  errno = ENOTSUP;
+  return NULL;
+}
 
 WASM_MISSING_LIBC_FUNC
 int umask(int mask) { return 0; }
@@ -54,19 +79,35 @@ WASM_MISSING_LIBC_FUNC
 int mprotect(const void *addr, size_t len, int prot) { return 0; }
 
 WASM_MISSING_LIBC_FUNC
-int pipe(int pipefd[2]) { return ENOTSUP; }
+int pipe(int pipefd[2]) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int posix_madvise(void *addr, size_t len, int advice) { return 0; }
+int posix_madvise(void *addr, size_t len, int advice) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int kill(pid_t pid, int sig) { return 0; }
+int kill(pid_t pid, int sig) {
+  errno = ENOTSUP;
+  return -1;
+}
+
 
 WASM_MISSING_LIBC_FUNC
 void tzset(void) { return; }
 
 WASM_MISSING_LIBC_FUNC
-int shutdown(int s, int how) { return ENOTSUP; }
+int shutdown(int s, int how) {
+  errno = ENOTSUP;
+  return -1;
+}
 
 WASM_MISSING_LIBC_FUNC
-int system(const char *command) { return ENOTSUP; }
+int system(const char *command) {
+  errno = ENOTSUP;
+  return -1;
+}
