da028e1fd8f4e30379e48cd3a175e191f9bb64a2
Yuta Saito <kateinoigakukun@gmail.com>
2021-12-09 13:27:51 +0900

    [wasm] test: revert disabled tests under ./test/

--- test/ruby/test_alias.rb
+++ test/ruby/test_alias.rb
@@ -202,10 +202,6 @@ class TestAlias < Test::Unit::TestCase
   end
 
   def test_alias_in_module
-    if /wasi/ =~ RUBY_PLATFORM
-      return
-    end
-
     bug9663 = '[ruby-core:61635] [Bug #9663]'
 
     assert_separately(['-', bug9663], "#{<<-"begin;"}\n#{<<-'end;'}")
