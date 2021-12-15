b7f51c23d8278ce02135c7658dfcc1853ea28337
Yuta Saito <kateinoigakukun@gmail.com>
2021-12-15 18:44:51 +0900

    [wasm] thread_none.{c,h}: improve comments

--- thread_none.h
+++ thread_none.h
@@ -8,12 +8,9 @@
 // based implementation in vm.c
 #define RB_THREAD_LOCAL_SPECIFIER
 
-typedef struct native_thread_data_struct {
-} native_thread_data_t;
+typedef struct native_thread_data_struct {} native_thread_data_t;
 
-
-typedef struct rb_global_vm_lock_struct {
-} rb_global_vm_lock_t;
+typedef struct rb_global_vm_lock_struct {} rb_global_vm_lock_t;
 
 RUBY_EXTERN struct rb_execution_context_struct *ruby_current_ec;
 
