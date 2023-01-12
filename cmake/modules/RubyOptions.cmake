2f5e799f01fd2df65a77f014b4864eb381eaa869
Yuta Saito <kateinoigakukun@gmail.com>
2023-01-12 06:20:52 +0000

    [cmake] compile coroutine sources

--- cmake/modules/RubyOptions.cmake
+++ cmake/modules/RubyOptions.cmake
@@ -1,3 +1,8 @@
 option(RUBY_ENABLE_YJIT
        "enable in-process JIT compiler that requires Rust build tools. enabled by default on supported platforms if rustc 1.58.0+ is available"
        FALSE)
+
+set(RUBY_COROUTINE_default "amd64")
+set(RUBY_COROUTINE
+    "${RUBY_COROUTINE_default}" CACHE STRING
+    "specify the coroutine implementation to use")
