04c6a341529a584f9b90fb45b7f4963b718ee836
Yuta Saito <kateinoigakukun@gmail.com>
2023-01-12 05:44:24 +0000

    [cmake] compile yjit.c only when RUBY_ENABLE_YJIT=TRUE

---
+++ cmake/modules/RubyOptions.cmake
@@ -0,0 +1,3 @@
+option(RUBY_ENABLE_YJIT
+       "enable in-process JIT compiler that requires Rust build tools. enabled by default on supported platforms if rustc 1.58.0+ is available"
+       FALSE)
