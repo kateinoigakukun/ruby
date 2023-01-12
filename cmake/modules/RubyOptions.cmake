2076da7de4a7e26da42bec5924f93223e1029de3
Yuta Saito <kateinoigakukun@gmail.com>
2023-01-12 06:29:48 +0000

    [cmake] compile dtrace sources

--- cmake/modules/RubyOptions.cmake
+++ cmake/modules/RubyOptions.cmake
@@ -6,3 +6,8 @@ set(RUBY_COROUTINE_default "amd64")
 set(RUBY_COROUTINE
     "${RUBY_COROUTINE_default}" CACHE STRING
     "specify the coroutine implementation to use")
+
+set(RUBY_ENABLE_DTRACE_default FALSE)
+option(RUBY_ENABLE_DTRACE
+       "enable DTrace for tracing inside ruby. enabled by default on systems having dtrace"
+       ${RUBY_ENABLE_DTRACE_default})
