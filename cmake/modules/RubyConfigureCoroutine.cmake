3ca5adb17c48637755baf5a539d6530c4fadcdd4
Yuta Saito <kateinoigakukun@gmail.com>
2023-01-13 17:14:22 +0000

    [cmake] set RUBY_SOURCE_DIR to avoid depending on cwd at configuration

--- cmake/modules/RubyConfigureCoroutine.cmake
+++ cmake/modules/RubyConfigureCoroutine.cmake
@@ -1,16 +1,17 @@
 function(ruby_configure_coroutine type src_out header_out)
-  set(header "coroutine/${type}/Context.h")
+  set(coroutine_dir "${RUBY_SOURCE_DIR}/coroutine/${type}")
+  set(header "${coroutine_dir}/Context.h")
 
   if(NOT EXISTS ${header})
     message(FATAL_ERROR "'${type}' is not supported as coroutine")
   endif()
 
-  set(src "coroutine/${type}/Context.c")
+  set(src "${coroutine_dir}/Context.c")
   if(NOT EXISTS ${src})
-    set(src "coroutine/${type}/Context.S")
+    set(src "${coroutine_dir}/Context.S")
   endif()
   if(NOT EXISTS ${src})
-    set(src "coroutine/${type}/Context.asm")
+    set(src "${coroutine_dir}/Context.asm")
   endif()
 
   set(${src_out} ${src} PARENT_SCOPE)
