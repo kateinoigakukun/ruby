3093960892094ec1c0669e6376c19cd355350005
Yuta Saito <kateinoigakukun@gmail.com>
2023-12-18 18:01:26 +0000

    [wasm-pic] Add `dl*` functions implementation

--- dln.c
+++ dln.c
@@ -515,6 +515,8 @@ dln_load(const char *file)
     char *init_fct_name;
     init_funcname(&init_fct_name, file);
 
+    // printf("%s:%d init_fct_name: %s\n", __FILE__, __LINE__, init_fct_name);
+
     /* Call the init code */
     dln_sym_callable(void, (void), handle, init_fct_name)();
 
@@ -540,3 +542,167 @@ dln_load(const char *file)
 
     return 0;			/* dummy return */
 }
+
+#if defined(__wasi__)
+// The following dl* functions implementation is based on @dicej's wasi-libc:
+// https://github.com/dicej/wasi-libc/blob/5362139d0aba8ea29899f2c6510a917e59344859/libc-top-half/musl/src/misc/dl.c
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+#define EXPORT __attribute__((visibility("default"))) __attribute__((used))
+
+#undef MIN
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
+struct name_t {
+    uint32_t length;
+    uint8_t *data;
+};
+
+struct symbol_t {
+    struct name_t name;
+    void *address;
+};
+
+struct symbols_t {
+    uint32_t count;
+    struct symbol_t *symbols;
+};
+
+struct library_t {
+    struct name_t name;
+    struct symbols_t symbols;
+};
+
+struct libraries_t {
+    uint32_t count;
+    struct library_t *libraries;
+};
+
+static const char *error = 0;
+static struct libraries_t *libraries = 0;
+
+static int invalid_handle(void *library_void) {
+    if (libraries == 0) {
+        fprintf(stderr, "`__wasm_set_libraries` should have been called during "
+                "instantiation with a non-NULL value\n");
+        abort();
+    }
+
+    for (uint32_t index = 0; index < libraries->count; ++index) {
+        if (libraries->libraries + index == library_void) {
+            return 0;
+        }
+    }
+
+    error = "invalid library handle";
+    return 1;
+}
+
+EXPORT int dlclose(void *library_void) {
+    if (invalid_handle(library_void)) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+EXPORT char *dlerror(void) {
+    const char *var = error;
+    error = 0;
+    return (char*) var;
+}
+
+EXPORT void *dlopen(const char *name, int flags) {
+    if (libraries == 0) {
+        fprintf(stderr, "`__wasm_set_libraries` should have been called during "
+                "instantiation with a non-NULL value\n");
+        abort();
+    }
+
+    if (flags & ~(RTLD_LAZY | RTLD_NOW | RTLD_GLOBAL)) {
+        // TODO
+        error = "dlopen flags not yet supported";
+        return 0;
+    }
+
+    uint32_t length = strlen(name);
+    int first = 0;
+    int last = libraries->count - 1;
+    while (first <= last) {
+        int middle = (first + last) / 2;
+        struct library_t *library = libraries->libraries + middle;
+        // printf("%s:%d checking %s\n", __FILE__, __LINE__, library->name.data);
+        int diff = memcmp(library->name.data, name, MIN(library->name.length, length));
+        if (diff == 0) {
+            if (library->name.length == length) {
+                return library;
+            } else if (library->name.length < length) {
+                first = middle + 1;
+            } else {
+                last = middle - 1;
+            }
+        } else if (diff < 0) {
+            first = middle + 1;
+        } else {
+            last = middle - 1;
+        }
+    }
+
+    error = "library not found";
+    return 0;
+}
+
+EXPORT void *dlsym(void *library_void, const char *name) {
+    error = NULL;
+    if (library_void == RTLD_NEXT || library_void == RTLD_DEFAULT) {
+        // TODO
+        error = "dlsym RTLD_NEXT and RTLD_DEFAULT not yet supported";
+        return 0;
+    }
+
+    if (invalid_handle(library_void)) {
+        return 0;
+    }
+
+    struct library_t *library = library_void;
+
+    uint32_t length = strlen(name);
+    int first = 0;
+    int last = library->symbols.count - 1;
+    // printf("%s:%d %s looking for %s in %s\n", __FILE__, __LINE__, __func__, name, library->name.data);
+    while (first <= last) {
+        int middle = (first + last) / 2;
+        struct symbol_t *symbol = library->symbols.symbols + middle;
+        // printf("%s:%d %s checking %s (%p)\n", __FILE__, __LINE__, __func__, symbol->name.data, symbol->address);
+        int diff = memcmp(symbol->name.data, name, MIN(symbol->name.length, length));
+        // printf("%s:%d %s diff %d\n", __FILE__, __LINE__, __func__, diff);
+        if (diff == 0) {
+            if (symbol->name.length == length) {
+                // printf("%s:%d %s found %s (%p)\n", __FILE__, __LINE__, __func__, symbol->name.data, symbol->address);
+                return symbol->address;
+            } else if (symbol->name.length < length) {
+                first = middle + 1;
+            } else {
+                last = middle - 1;
+            }
+        } else if (diff < 0) {
+            first = middle + 1;
+        } else {
+            last = middle - 1;
+        }
+    }
+
+    error = "symbols not found";
+    return 0;
+}
+
+EXPORT void __wasm_set_libraries(struct libraries_t *new_libraries) {
+    libraries = new_libraries;
+}
+
+#endif /* __wasi__ */
